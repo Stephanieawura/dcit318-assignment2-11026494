042306a199b7d9a3b5a6aa51bd4e9180e5c3766e
Stephanie Awurabena <stephanieawurabena99@gmail.com>
2025-08-06 18:59:43 +0000

    add Inheritance and Method Overriding

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        genericAnimal.MakeSound(); // prints "Some generic sound"
+        dog.MakeSound();           // prints "Bark"
+        cat.MakeSound();           // prints "Meow"
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move(); // only a declaration
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();       // prints "Car is moving"
+        bicycle.Move();   // prints "Bicycle is moving"
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
